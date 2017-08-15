6ad91e519af91b9f4713fc8a69093be41b25c891
zar-rok <zar_rok@live.fr>
2017-08-16 00:22:41 +0200

    opti with pixel array and clean code

--- gol.cpp
+++ gol.cpp
@@ -8,144 +8,139 @@
 size_t number_neighbours(std::vector<bool> &grid, const size_t x, const size_t y, const size_t nbr_cell_x, const size_t nbr_cell_y)
 {
   size_t nbr_neighbours = 0;
-  
+
   // North
   bool is_north_ok = y > 0;
   size_t north = y - 1;  
-  if (is_north_ok && grid[x + north * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_north_ok && grid[north + x * nbr_cell_y]) nbr_neighbours++;
+
   // East
   bool is_east_ok = x < (nbr_cell_x - 1);
   size_t east = x + 1;
-  if (is_east_ok && grid[east + y * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_east_ok && grid[y + east * nbr_cell_y]) nbr_neighbours++;
+
   // South
   bool is_south_ok = y < (nbr_cell_y - 1);
   size_t south = y + 1;
-  if (is_south_ok && grid[x + south * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_south_ok && grid[south + x * nbr_cell_y]) nbr_neighbours++;
+
   // West
   bool is_west_ok = x > 0;
   size_t west = x - 1;
-  if (is_west_ok && grid[west + y * nbr_cell_x]) nbr_neighbours++;
+  if (is_west_ok && grid[west + y * nbr_cell_y]) nbr_neighbours++;
 
   // North - East
-  if (is_north_ok && is_east_ok && grid[east + north * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_north_ok && is_east_ok && grid[north + east * nbr_cell_y]) nbr_neighbours++;
+
   // North - West
-  if (is_north_ok && is_west_ok && grid[west + north * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_north_ok && is_west_ok && grid[north + west * nbr_cell_y]) nbr_neighbours++;
+
   // South - East
-  if (is_south_ok && is_east_ok && grid[east + south * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_south_ok && is_east_ok && grid[south + east * nbr_cell_y]) nbr_neighbours++;
+
   // South - West
-  if (is_south_ok && is_west_ok && grid[west + south * nbr_cell_x]) nbr_neighbours++;
-  
+  if (is_south_ok && is_west_ok && grid[south + west * nbr_cell_y]) nbr_neighbours++;
+
   return nbr_neighbours;
 }
 
-size_t randomize(std::vector<bool> &grid)
-{  
-  size_t nbr_cell_used = 0;
+void randomize(std::vector<bool> &grid)
+{
   for (size_t i = 0; i < grid.size(); ++i)
-  {
-    if ((rand() % 100) > 80)
-    {
-      grid[i] = true;
-      nbr_cell_used++;
-    }
-    else
-    {
-      grid[i] = false;
-    }
-  }
-  
-  return nbr_cell_used;
+    grid[i] = (rand() % 100) > 80;
 }
 
 std::vector<bool> next_generation(std::vector<bool> &grid, const size_t nbr_cell_x, const size_t nbr_cell_y)
 {
   size_t nbr_neig = 0;
   std::vector<bool> new_grid = std::vector<bool>(grid.size());
-  for (size_t i = 0; i < nbr_cell_x; ++i)
+  for (size_t row = 0; row < nbr_cell_x; ++row)
   {
-    for (size_t j = 0; j < nbr_cell_y; ++j)
+    for (size_t col = 0; col < nbr_cell_y; ++col)
     {
-      nbr_neig = number_neighbours(grid, i, j, nbr_cell_x, nbr_cell_y);
-      
+      nbr_neig = number_neighbours(grid, row, col, nbr_cell_x, nbr_cell_y);
       // Living cell
-      if (grid[i + j * nbr_cell_x])
+      if (grid[col + row * nbr_cell_y])
       {
-        // Underpopulation || overpopulation
+        // Underpopulation or Overpopulation = death
         if (nbr_neig < 2 || nbr_neig > 3)
-          new_grid[i + j * nbr_cell_x] = false;
+          new_grid[col + row * nbr_cell_y] = false;
+        else
+          new_grid[col + row * nbr_cell_y] = true;
       }
       else
+      // Empy cell
       {
+        // Give life
         if (nbr_neig == 3)
-          new_grid[i + j * nbr_cell_x] = true;
+          new_grid[col + row * nbr_cell_y] = true;
       }
     }
   }
-  
   return new_grid;
 }
 
-void generate_quads(std::vector<sf::VertexArray*> &quads, std::vector<bool> &grid, const size_t nbr_cell_x, const size_t nbr_cell_y, const size_t cell_width, const size_t cell_height)
+void place_pixels(sf::Uint8 *pixels, const std::vector<bool> &grid, const size_t window_height, const size_t nbr_cell_x, const size_t nbr_cell_y, const size_t cell_width, const size_t cell_height)
 {
-  size_t pos_x = 0;
-  size_t pos_y = 0;
-  size_t index = 0;
+  sf::Color dead = sf::Color::White;
+  sf::Color alive = sf::Color::Black;
+  bool is_used = false;
+  size_t pixel = 0;
+  size_t offset_x = 0;
+  size_t offset_y = 0;
   for (size_t i = 0; i < nbr_cell_x; ++i)
   {
-    pos_y = 0;
     for (size_t j = 0; j < nbr_cell_y; ++j)
     {
-      if (grid[i + j * nbr_cell_x])
-      {        
-        quads[index] = new sf::VertexArray(sf::Quads, 4);
-        
-        (*quads[index])[0].position = sf::Vector2f(pos_x, pos_y);
-        (*quads[index])[1].position = sf::Vector2f(pos_x + cell_width, pos_y);
-        (*quads[index])[2].position = sf::Vector2f(pos_x + cell_width, pos_y + cell_height);
-        (*quads[index])[3].position = sf::Vector2f(pos_x, pos_y + cell_height);
-        
-        (*quads[index])[0].color = sf::Color::Black;
-        (*quads[index])[1].color = sf::Color::Black;
-        (*quads[index])[2].color = sf::Color::Black;
-        (*quads[index])[3].color = sf::Color::Black;
-        
-        index++;
+      is_used = grid[j + i * nbr_cell_y];
+      offset_x = i * cell_width * 4;
+      offset_y = j * cell_height * 4;
+      for (size_t row = 0; row < cell_width; ++row)
+      {
+        for (size_t col = 0; col < cell_height; ++col)
+        {
+          pixel = (offset_y + row * 4) + (offset_x + col * 4) * window_height;
+          pixels[pixel] = (is_used) ? alive.r : dead.r;
+          pixels[pixel + 1] = (is_used) ? alive.g : dead.g;
+          pixels[pixel + 2] = (is_used) ? alive.b : dead.b;
+          pixels[pixel + 3] = (is_used) ? alive.a : dead.a;
+        }
       }
-      pos_y += cell_height;
     }
-    pos_x += cell_width;
   }
 }
 
 int main()
 {
+  //sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+  sf::RenderWindow window(sf::VideoMode(800, 800), "GOL"/*, sf::Style::Fullscreen*/);
+  sf::Vector2u window_size = window.getSize();
+  size_t window_width = window_size.x;
+  size_t window_height = window_size.y;
+
   srand(time(NULL));
-  
-  int window_width = 800;
-  int window_height = 600;
-  
-  size_t cell_width = 10;
-  size_t cell_height = 10;
-  
+
+  size_t cell_width = 5;
+  size_t cell_height = 5;
+
   size_t nbr_cell_x = window_width / cell_width;
   size_t nbr_cell_y = window_height / cell_height;
   size_t capacity = nbr_cell_x * nbr_cell_y;
 
-  sf::RenderWindow window(sf::VideoMode(window_width, window_height), "GOL");
-  window.setFramerateLimit(60);
-  
   std::vector<bool> grid = std::vector<bool>(capacity);
-  size_t nbr_cell_used = randomize(grid);
-  
-  std::vector<sf::VertexArray*> quads = std::vector<sf::VertexArray*>(nbr_cell_used);
-  generate_quads(quads, grid, nbr_cell_x, nbr_cell_y, cell_width, cell_height);
-  
+  randomize(grid);
+
+  size_t pixels_size = window_width * window_height * 4;
+  sf::Uint8 pixels[pixels_size];
+
+  place_pixels(pixels, grid, window_width, nbr_cell_x, nbr_cell_y, cell_width, cell_height);
+
+  sf::Texture texture;
+  texture.create(window_width, window_height);
+  sf::Sprite sprite(texture);
+  texture.update(pixels);
+
+  sf::Clock clock;
   while (window.isOpen())
   {
     sf::Event event;
@@ -167,22 +162,30 @@ int main()
         default: break;
       }
     }
-    
-    window.clear(sf::Color::White);
-    
-    for (size_t i = 0; i < quads.size(); ++i) {
-      window.draw(*quads[i]);
-    }
-    
+    //printf("[*] Event : %d ms\n", clock.getElapsedTime().asMilliseconds());
+
+    //window.clear(sf::Color::White);
+
+    window.draw(sprite);
+
     window.display();
-    
-    grid = next_generation(grid, nbr_cell_x, nbr_cell_y);
-    generate_quads(quads, grid, nbr_cell_x, nbr_cell_y, cell_width, cell_height);
+
+    //printf("[*] Draw & Display: %d ms\n", clock.getElapsedTime().asMilliseconds());
+
+    if (clock.getElapsedTime().asMilliseconds() >= 0)
+    {
+      grid = next_generation(grid, nbr_cell_x, nbr_cell_y);
+
+      place_pixels(pixels, grid, window_width, nbr_cell_x, nbr_cell_y, cell_width, cell_height);
+
+      texture.update(pixels);
+
+      clock.restart();
+    }
+
+    //printf("[*] Compute next: %d ms\n", clock.getElapsedTime().asMilliseconds());
+
+    //clock.restart();
   }
-  
-  for (size_t i = 0; i < quads.size(); ++i) {
-    delete quads[i];
-  }
-  
   return 0;
 }
